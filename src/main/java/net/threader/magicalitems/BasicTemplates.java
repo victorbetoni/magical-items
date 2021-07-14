52adf1508724faec9fac6081b02452951aaa3e34
threader <40972803+localthreader@users.noreply.github.com>
2021-07-14 17:11:42 -0300

    Implementing template casters and registry

--- src/main/java/net/threader/magicalitems/BasicTemplates.java
+++ src/main/java/net/threader/magicalitems/BasicTemplates.java
@@ -1,13 +1,20 @@
 package net.threader.magicalitems;
 
+import net.threader.magicalitems.registry.TemplateRegister;
 import net.threader.magicalitems.template.LivingEntityActionTemplate;
 import org.bukkit.potion.PotionEffect;
 
+import java.util.function.Function;
+
 public class BasicTemplates {
-    public static LivingEntityActionTemplate applyEffect =
-            new LivingEntityActionTemplate("apply_effects", (p, i, e, a) -> {
-                for (Object o : a) {
-                    e.addPotionEffect((PotionEffect) o);
-                }
-    });
+
+    public static final TemplateRegister REGISTER = new TemplateRegister();
+
+    public static final Function<Object[], LivingEntityActionTemplate> APPLY_EFFECTS = (args) ->
+            (LivingEntityActionTemplate) REGISTER.register(
+                    new LivingEntityActionTemplate("apply_effects", (p, i, e, a) -> {
+                        for (Object o : a) {
+                            e.addPotionEffect((PotionEffect) o);
+                        }
+                    }, args));
 }
--- src/main/java/net/threader/magicalitems/template/LivingEntityActionTemplate.java
+++ src/main/java/net/threader/magicalitems/template/LivingEntityActionTemplate.java
@@ -6,10 +6,12 @@ import org.bukkit.entity.LivingEntity;
 
 public class LivingEntityActionTemplate extends ActionTemplate {
     private Action<LivingEntity> action;
+    private Object[] args;
 
-    public LivingEntityActionTemplate(String id, Action<LivingEntity> action) {
+    public LivingEntityActionTemplate(String id, Action<LivingEntity> action, Object[] args) {
         super(id);
         this.action = action;
+        this.args = args;
     }
 
     public Action<LivingEntity> getAction() {
