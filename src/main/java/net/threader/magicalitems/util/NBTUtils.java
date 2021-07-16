814aa3d17d96a1444f90694c047cc57f5be574aa
threader <40972803+localthreader@users.noreply.github.com>
2021-07-16 19:45:39 -0300

    Using persistent data containers instead of raw NBT data

--- src/main/java/net/threader/magicalitems/util/NBTUtils.java
+++ src/main/java/net/threader/magicalitems/util/NBTUtils.java
@@ -1,26 +1,33 @@
 package net.threader.magicalitems.util;
 
 import net.minecraft.server.v1_16_R3.NBTTagCompound;
+import net.threader.magicalitems.MagicalItems;
+import org.bukkit.NamespacedKey;
 import org.bukkit.craftbukkit.v1_16_R3.inventory.CraftItemStack;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class NBTUtils {
+
+    private static final NamespacedKey MAGICAL_KEY = new NamespacedKey(MagicalItems.instance(), "magical-item-id");
+
     public static boolean isMagical(ItemStack stack) {
-        net.minecraft.server.v1_16_R3.ItemStack craftStack = CraftItemStack.asNMSCopy(stack);
-        NBTTagCompound compound = craftStack.getTag();
-        return compound != null && compound.getKeys().contains("magical_item_id");
+        ItemMeta meta = stack.getItemMeta();
+        return meta.getPersistentDataContainer().has(MAGICAL_KEY, PersistentDataType.STRING);
     }
 
     public static String extractIdentifier(ItemStack stack) {
-        net.minecraft.server.v1_16_R3.ItemStack craftStack = CraftItemStack.asNMSCopy(stack);
-        NBTTagCompound compound = craftStack.getTag();
-        return compound.getString("magical_item_id");
+        ItemMeta meta = stack.getItemMeta();
+        return meta.getPersistentDataContainer().get(MAGICAL_KEY, PersistentDataType.STRING);
     }
 
     public static void injectIdentifier(ItemStack stack, String id) {
-        net.minecraft.server.v1_16_R3.ItemStack craftStack = CraftItemStack.asNMSCopy(stack);
-        NBTTagCompound compound = craftStack.hasTag() ? craftStack.getTag() : new NBTTagCompound();
-        compound.setString("magical_item_id", id);
-        craftStack.setTag(compound);
+        ItemMeta meta = stack.getItemMeta();
+        meta.getPersistentDataContainer().set(MAGICAL_KEY, PersistentDataType.STRING, id);
+        stack.setItemMeta(meta);
     }
 }
